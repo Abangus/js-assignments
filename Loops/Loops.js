0900c6b511b71d1e6d0ef129830f6f9e5f0d386a
Abangus <angus182023.liu@gmail.com>
2023-02-06 23:15:24 +0800

    Loop

---
+++ Loops/Loops.js
@@ -0,0 +1,45 @@
+let love = 0
+let num = 0
+let odd = 12
+let square = 0
+let picture = 4
+let tri = 0
+let tower = 7
+
+for (love = 0; love < 5; love++) {
+    document.write ('I love web dev class ')
+    document.write('<br/>');
+}
+
+for (num = 0; num < 5; num++ ) {
+    document.write (num)
+    document.write('<br/>');
+}
+
+for (odd = 15; odd < 33; odd = odd + 3) {
+    document.write (odd)
+    document.write('<br/>');
+}
+
+for (square = 0; square < 4; square++) {
+    for(picture = 4; picture > 0; picture-- ) {
+        document.write('nnn ')
+    }
+    document.write('<br/>');
+}
+
+for (tri = 0; tri < 7; tri++) {
+    for(tower = 0; tower <= tri; tower++ ) {
+        document.write('bone')
+    }
+    document.write('<br/>');
+}
+
+for (tri = 0; tri < 7; tri++) {
+    for(tower = 0; tower <= tri; tower++ ) {
+        document.write('bone')
+    }
+    document.write('<br/>');
+}
+
+
